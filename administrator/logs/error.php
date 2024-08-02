eb9f4405f3de33fc618f016282de9d9b0bc00930
marshall9213 <marshall1992.al@gmail.com>
2024-08-02 11:33:37 +0200

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-07-29 15:03:37 UTC
+#Software: Joomla! 5.1.2 Stable [ Kudumisha ] 9-July-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-07-29T15:03:37+00:00	INFO 192.168.1.11	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-07-29T17:09:07+00:00	INFO 192.168.1.11	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/joomla_scheduler.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-07-29 14:54:08 UTC
+#Software: Joomla! 5.1.2 Stable [ Kudumisha ] 9-July-2024 16:00 GMT
+
+#Fields: date	time	priority	message
+2024-07-29	14:54:08	INFO	Running task#03 'Update Notification'.
+2024-07-29	14:54:19	INFO	Successfully finished task#03 in 10.77 (net 10.81) seconds.
+2024-07-29	14:55:03	INFO	Running task#02 'Session GC'.
+2024-07-29	14:55:03	INFO	Task> SessionGC end
+2024-07-29	14:55:03	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
